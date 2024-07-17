1c45322addd73e98487e821aab18822b8924f6e9
Philipp Staender <pstaender@mailbox.org>
2024-07-17 06:59:57 +0200

    adds file context option

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Diggit
-## Let ai generate your git commands
+## Generates git commands from natural language
 
 ### Install
 
@@ -7,27 +7,38 @@
 $ npm install -g diggit
 ```
 
-Set your api secret as (global) `OPENAI_API_KEY`-env-variable.
+Set your api key as (global) `OPENAI_API_KEY`-env-variable.
 
 ### Usage
 
 ```sh
-$ diggit - Your problem for what you need a git command
+$ diggit - Generate git command from sentence
+```
+
+```sh
+$ diggit -- Generate git command from sentence but also sends relevant filenames as context
 ```
 
 ### Examples
 
 ```sh
 $ diggit - Unstage the file docker-compose.yml
+git reset HEAD docker-compose.yml
 ```
 
 ```sh
-$ diggit - Add lines of the file docker-compose.yml interactively
+$ diggit - Add and commit lines from docker-compose.yml interactively
+git add -p docker-compose.yml && git commit
+```
+
+```sh
+$ diggit -- Add and commit all models
+git add app/models && git commit
 ```
 
 ### yolo
 
-⚠️ If you have nothing to lose, feel free to execute the command directly and accept the potential consequences ⚠️
+If you have nothing to lose, feel free to execute the command directly and accept the potential consequences:
 
 ```sh
 $ diggit - Add lines of the file docker-compose.yml interactively|sh
--- diggit.mjs
+++ diggit.mjs
@@ -2,20 +2,26 @@
 
 import { generateText } from "ai";
 import { openai } from "@ai-sdk/openai"; // Ensure OPENAI_API_KEY environment variable is set
+import { execFileSync } from 'node:child_process';
+import { readFileSync } from "node:fs";
 
-async function main(command) {
+async function main(command, relevantFiles = []) {
+
+  let relevantFilesPrompt = relevantFiles.length > 0 ? `We have the following files in a git repo:\n\n${relevantFiles.join(',')}\n\n` : '';
+
   const { text } = await generateText({
     model: openai("gpt-4o"),
-    prompt: `Please generate and return only the git command for: ${command.trim()}`
+    prompt: `${relevantFilesPrompt}Please generate and return only the git command for: ${command.trim()}`
   });
   return text
     .trim()
-    .replace(/^```(bash|sh|shell)/, "")
-    .replace(/```$/, "")
+    .split('```')[1]
+    .replace(/^(bash|shell|sh)/, '')
     .trim();
 }
 
 let args = null;
+let relevantFiles = [];
 
 for (let arg of process.argv) {
   if (args?.constructor === Array) {
@@ -23,18 +29,27 @@ for (let arg of process.argv) {
     continue;
   }
   if (arg === "--help" || arg === "-h") {
+    let packageJson = JSON.parse(readFileSync(new URL('./package.json', import.meta.url)));
     console.log(
-      `Type your git issue and get the command to get it done (c) philipp staender 2024\nUsage: diggit Unstage file xyz.js`
+      `Generates git commands from natural language v${packageJson.version}\nUsage: diggit - Unstage file xyz.js`
     );
   }
   if (arg === "-") {
     args = [];
   }
+  if (arg === "--") {
+    args = [];
+    relevantFiles = process.cwd() ? execFileSync('git', ['diff', '--name-only'], {
+      stdio: 'pipe',
+      cwd: process.cwd(),
+      encoding: 'utf8',
+    }).split("\n").filter(l => !!l).slice(0, 80) : [];
+  }
 }
 
 (async () => {
   if (args && args.join("").trim()) {
-    let gitCommand = await main(args.join(" "));
+    let gitCommand = await main(args.join(" "), relevantFiles);
     console.log(gitCommand);
   }
 })();
